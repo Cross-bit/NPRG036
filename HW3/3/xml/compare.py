a6bb97a9b5406026f90aa5e8a35e5e988f89cdf2
Cross-bit <ondra.kryz@gmail.com>
2022-12-08 23:36:30 +0100

    add python script for merged results

---
+++ HW3/3/xml/compare.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+from rdflib import Graph
+from rdflib.compare import *
+
+mergeRdfs = True
+
+HOME = os.getenv('HOME')
+
+original_rdf_path = HOME + "/school/NPRG036/HW2/2/rdf/data/data.ttl"
+#merged_rdf_path = HOME + "/school/NPRG036/HW3/3/xml/rdf-merged.ttl"
+merged_rdf_path = HOME + "/school/NPRG036/HW3/TOBEDONE.ttl"
+
+if mergeRdfs:
+    command = 'bash merge.sh'
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+
+original_g = Graph()
+original_g.parse(original_rdf_path)
+
+merged_g = Graph()
+merged_g.parse(merged_rdf_path)
+
+
+# launch smart algorithm
+original_iso = to_isomorphic(original_g)
+merged_iso = to_isomorphic(merged_g)
+
+if original_iso == merged_iso:
+    print("OK")
+else:
+    (both, original, merged) = graph_diff(original_g, merged_g)
+    print("Missing tripples:")
+    for s, p, o in original:
+        print(s, p, o);
+
+
+
+
+#for s, p, o in g:
+#    print(s, p, o)
