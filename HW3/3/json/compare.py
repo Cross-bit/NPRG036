d7bbc2a9091142abe55d17c54b8e095e985342b6
Cross-bit <ondra.kryz@gmail.com>
2022-12-11 20:16:18 +0100

    add generated data

---
+++ HW3/3/json/compare.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+from rdflib import Graph
+from rdflib.compare import *
+
+HOME = os.getenv('HOME')
+
+# nastavete si svoje cesty... HOME + "/school/NPRG036/HW2/2/rdf/data/data.ttl"
+original_rdf_path = HOME + "/school/NPRG036/HW3/3/xml/rdf/data-5.ttl"
+merged_rdf_path = HOME + "/school/NPRG036/HW3/3/json/rdf/data-5.nq" #rdf-merged
+#merged_rdf_path = HOME + "/school/NPRG036/HW3/TOBEDONE.ttl"
+
+original_g = Graph()
+original_g.parse(original_rdf_path)
+
+jsonld_g = Graph()
+jsonld_g.parse(merged_rdf_path)
+
+
+# launch smart algorithm
+original_iso = to_isomorphic(original_g)
+merged_iso = to_isomorphic(jsonld_g)
+
+if original_iso == merged_iso:
+    print("OK")
+else:
+    (both, original, jsonld) = graph_diff(original_g, jsonld_g)
+    print("Missing tripples:")
+    for s, p, o in original:
+        print(s, p, o)
+    print("Useless tripples:")
+    for s, p, o in jsonld:
+        print(s, p, o)
---
+++ HW3/3/json/complete.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+for i in {1..5}
+do
+    bash generate.sh $i
+done
+
+#bash merge.sh
+
+
+
+
+
