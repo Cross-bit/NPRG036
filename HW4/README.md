e24f82684ae0f41936dc08af021b31f6554c7651
SatXG <116314954+SatXG@users.noreply.github.com>
2022-12-13 09:51:22 +0100

    HW4 kombinace

--- HW4/README.md
+++ HW4/README.md
@@ -1,5 +1,11 @@
 # [HW4](https://jakub.xn--klmek-0sa.com/nprg036-hw4)
 
+## Instrukce
+
+- Deskriptory tabulek pište do souborů ve složce `metadata`.
+- Vlastní prefixy pište do `prefixes.txt`.
+- **JSON-LD deskriptory obecně nepodporují definice vlastních prefixů**, ale spojovací skript v textu nahradí ty definované v `prefixes.txt`.
+
 ## Skripty (bash/ubuntu)
 
 ```
@@ -12,10 +18,10 @@ Nainstaluje Ruby a konvertor/validátor.
 ./gen.sh [-c]
 ```
 
-Zvaliduje a vygeneruje. `-c` taky vypíše RDF.
+Spojí deskriptory, nahradí prefixy a zvaliduje a vygeneruje. `-c` taky vypíše RDF.
 
 **Prefixy se musí specifikovat v `prefixes.txt`**
 
 ## CSV jsonld schema notes
 
-Pozor na celková URI v JSONld! Je potřeba uvést celé iri, ne jen prefixované...
+~~Pozor na celková URI v JSONld! Je potřeba uvést celé iri, ne jen prefixované...~~
--- HW4/gen.sh
+++ HW4/gen.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+metafile='4/csv/data.csv-metadata.json'
+
 getPrefs() {
 	{
 		cat prefixes.txt | sed -E "s/\s+/$2/"
@@ -11,9 +14,20 @@ getPrefs() {
 	}
 }
 
-if rdf validate --input-format tabular 4/csv/data.csv-metadata.json; then
-	
-	rdf serialize --input-format tabular --output-format turtle --minimal --decode-uri --prefixes "$(getPrefs ',' ':' '' | sed 's/^,//')" 4/csv/data.csv-metadata.json > 4/rdf/data.ttl
+{
+	cat 'data.csv-metadata.json-prefix'
+	sep=''
+	for td in metadata/*; do
+		echo "$sep"
+		sep=','
+		cat "$td" | sed "s/^/\t\t/$(getPrefs ';s,"' ':,"' ',')"
+	done
+	echo
+	cat 'data.csv-metadata.json-suffix'
+} > "$metafile"
+
+if rdf validate --input-format tabular "$metafile"; then
+	rdf serialize --input-format tabular --output-format turtle --minimal --decode-uri --prefixes "$(getPrefs ',' ':' '' | sed 's/^,//')" "$metafile" > 4/rdf/data.ttl
 	
 	if [ "$#" -gt 0 ] && [ "$1" = '-c' ]; then
 		echo
