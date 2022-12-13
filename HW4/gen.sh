757d939cc5e29215126f350fb54f35d0c9fd8b60
SatXG <116314954+SatXG@users.noreply.github.com>
2022-12-13 09:12:33 +0100

    Fix gen.sh

--- HW4/gen.sh
+++ HW4/gen.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-if rdf validate --input-format tabular 4/csv/data.csv-metadata.json; then
-	prefs="$({
-		cat prefixes.txt | sed -E 's/\s+/:/'
+getPrefs() {
+	{
+		cat prefixes.txt | sed -E "s/\s+/$2/"
 		echo
 	} | {
 		while read -r ln; do
-			echo -n ",$ln"
+			echo -n "$1$ln$3"
 		done
-	} | sed 's/^,//')"
+	}
+}
+
+if rdf validate --input-format tabular 4/csv/data.csv-metadata.json; then
 	
-	rdf serialize --input-format tabular --output-format turtle --minimal --decode-uri --prefixes ex:http://example.org/vocabulary/ 4/csv/data.csv-metadata.json > 4/rdf/data.ttl
+	rdf serialize --input-format tabular --output-format turtle --minimal --decode-uri --prefixes "$(getPrefs ',' ':' '' | sed 's/^,//')" 4/csv/data.csv-metadata.json > 4/rdf/data.ttl
 	
-	if [ "$#" > 0 ] && [ "$1" = '-c' ]; then
+	if [ "$#" -gt 0 ] && [ "$1" = '-c' ]; then
 		echo
 		cat 4/rdf/data.ttl | sed 's/^/\| /'
 	fi
